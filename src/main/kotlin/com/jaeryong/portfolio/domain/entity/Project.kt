96ba464fe3cf9b94a3f325c96d81d9738d99c728
jjr7181 <jjr7181@gmail.com>
2024-10-12 18:23:16 +0900

    엔티티 개발 - 연관관계 있음

--- src/main/kotlin/com/jaeryong/portfolio/domain/entity/Project.kt
+++ src/main/kotlin/com/jaeryong/portfolio/domain/entity/Project.kt
@@ -1,18 +1,75 @@
 package com.jaeryong.portfolio.domain.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 
 @Entity
-class Project: BaseEntity() {
+class Project(
+    name: String,
+    description: String,
+    startYear: Int,
+    startMonth: Int,
+    endYear: Int?,
+    endMonth: Int?,
+    isActive: Boolean
+): BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "project_id")
     var id: Long? = null
 
+    var name: String = name
+
+    var description: String = description
+
+    var startYear: Int = startYear
+
+    var startMonth: Int = startMonth
+
+    var endYear: Int? = null
+
+    // ? == nullable 하다는 뜻
+    var endMonth: Int? = null
+
+    var isActive: Boolean = isActive
+
+    @OneToMany(targetEntity = ProjectDetail:: class,
+        fetch = FetchType.LAZY,
+        cascade = [CascadeType.ALL])
+    @JoinColumn(name = "project_id")
+    var details: MutableList<ProjectDetail> = mutableListOf()
+
+    @OneToMany(mappedBy = "project")
+    var skills : MutableList<ProjectSkill> = mutableListOf()
+
+    fun getEndYearMonth(): String{
+        if (endYear == null || endMonth == null) {
+            return "Present"
+        }
+
+        return "${endYear}.${endMonth}" //2023.11
+    }
+
+    fun update(name: String,
+               description: String,
+               startYear: Int,
+               startMonth: Int,
+               endYear: Int?,
+               endMonth: Int?,
+               isActive: Boolean){
+        this.name = name
+        this.description = description
+        this.startYear = startYear
+        this.startMonth = startMonth
+        this.endYear = endYear
+        this.endMonth = endMonth
+        this.isActive = isActive
+    }
+
+    fun addDetails(details: MutableList<ProjectDetail>?) {
+        if (details != null) {
+            this.details.addAll(details)
+        }
+    }
 }
